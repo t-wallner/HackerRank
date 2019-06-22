97ec21ba7a31b10889cfb7105d42cd90a31e3c82
t-wallner <thomasawallner@gmail.com>
2019-06-22 16:08:13 +0100

    Fraudulent Activity Challenge added

--- Interview Prep Kit/Sort.py
+++ Interview Prep Kit/Sort.py
@@ -1,3 +1,61 @@
+############# HackerRank Challenge: Fraudulent Activity Notifications ############
+from collections import deque
+import numpy as np
+
+
+def activity_notifications(expenditures, trailing_days):
+    """ Given the number of trailing days d and a client's total daily expenditures
+        for a period of n days, find and print the number of times the client will receive
+        a notification over all n days. Allowed expenditures are non-negative integers up to 200"""
+    expenditures_in_period = deque()
+    frequency_distribution = np.zeros(201)
+
+    notice_count = 0
+    for i in range(0, len(expenditures)):
+
+        expenditure = expenditures[i]
+        # Populate expenditures in peiod and freq distr before trailing_days days are reached
+        if i < trailing_days:
+            expenditures_in_period.append(expenditure)
+            frequency_distribution[expenditure] += 1
+
+        else:
+            # Notice will be sent if current expenditure is more than 2*median of trailing expenditures
+            if expenditure >= median(frequency_distribution) * 2:
+                notice_count += 1
+
+            # Remove oldest expenditure from expenditures in period and freq distribution
+            frequency_distribution[expenditures_in_period.popleft()] -= 1
+
+            # Add newest expenditure to expenditures in period and to frequency distribution
+            expenditures_in_period.append(expenditure)
+            frequency_distribution[expenditure]
+
+    return notice_count
+
+
+def median(freq_distribution):
+    """ Calculates the median of a frequency distribution. Uses the size
+        of the distribution, which allows for faster calculations when l is uneven"""
+    sum_distribution = sum(freq_distribution)
+    count = 0
+
+    for value, freq in enumerate(freq_distribution):
+        count += freq
+        if count > sum_distribution/2:
+            break
+    # for un even case, median is just the middle value
+    if sum_distribution % 2 == 1:
+        return value
+    # for even case, median is average of two middle values
+    else:
+        next_non_zero_value = next((v for v, f in enumerate(freq_distribution[value + 1:]) if f), None)
+        return (value + next_non_zero_value) / 2
+
+
+############# HackerRank Challenge:  Comparator ############
+
+
 class Player:
 
     def __init__(self, name, score):
--- Interview Prep Kit/Tests.py
+++ Interview Prep Kit/Tests.py
@@ -1,8 +1,41 @@
 import unittest
 from Sort import Player
+from Sort import activity_notifications
+from Sort import median
+import numpy as np
 
 
-class TestSorting(unittest.TestCase):
+class TestSortingActivityMonitor(unittest.TestCase):
+
+    def test_activity_Notifications_SampleInput1_Returns2(self):
+        expenditures = [10, 20, 30, 40, 50]
+        trailing_days = 3
+
+        self.assertEqual(1, activity_notifications(expenditures, trailing_days))
+
+    def test_activity_Notifications_SampleInput2_Returns2(self):
+        expenditures = [2, 3, 4, 2, 3, 6, 8, 4, 5]
+        trailing_days = 5
+
+        self.assertEqual(2, activity_notifications(expenditures, trailing_days))
+
+    def test_activity_Notifications_SampleInput3_Returns2(self):
+        expenditures = [1, 2, 3, 4, 4]
+        trailing_days = 4
+
+        self.assertEqual(0, activity_notifications(expenditures, trailing_days))
+
+    def test_median_distribution_even(self):
+        freq_distribution = np.array([1, 1, 0, 0, 1, 0, 1])
+
+        self.assertEqual(2.5, median(freq_distribution))
+
+    def test_median_distribution_odd(self):
+        freq_distribution = np.array([1, 0, 1, 1, 2])
+
+        self.assertEqual(3, median(freq_distribution))
+
+class TestSortingComparator(unittest.TestCase):
 
     def test_comparator_WhenAScoresHigher_ReturnsNeg1(self):
         player_a = Player("Thomas", 100)
